4ea7b514414ededb0e90049d8d80542e7dd0205c
sescobar-2020031 <sescobar-2020031@kinal.edu.gt>
2022-07-28 15:03:21 -0600

    Se agrego previewPrescription y prescription

--- src/controllers/previewPrescription.controller.js
+++ src/controllers/previewPrescription.controller.js
@@ -34,126 +34,29 @@ exports.savePrescriptoionADMIN = async (req, res) => {
         const doctorExist = await Doctor.findOne({ _id: params.doctor });
         if (!doctorExist) return res.send({ message: 'Doctor no encontrado o no existe.' });
 
-        if (!previewPrescriptionExist) {
-            //Observa si los medicamentos existen
-            var medicamentExistStatus = true;
-            var laboratoryExistStatus = true;
-            const data = {
-                pacient: params.pacient,
-                doctor: params.doctor,
-            }
-            if (!medicamentExistStatus) {
-                res.status(400).send({ message: 'El medicamento no existe' });
-            } else if (!laboratoryExistStatus) {
-                res.status(400).send({ message: 'El laboratorio no existe' })
-            }
-            if (medicamentExistStatus) {
-                const medicamentoData = {
-                    name: null,
-                    description: null,
-                    medicament: null
-                };
-                if (params.name) {
-                    medicamentoData.name = params.nameMedicament;
-                    medicamentoData.description = params.descriptionMedicament;
-                } else if (params.medicament) {
-                    medicamentoData.medicament = params.medicament;
-                }
-                data.medicament = medicamentoData;
-            }
-            if (laboratoryExistStatus) {
-                const laboratoryData = {
-                    name: null,
-                    description: null,
-                    laboratory: null
-                };
-                if (params.nameLaboratory) {
-                    laboratoryData.name = params.nameLaboratory;
-                    laboratoryData.description = params.descriptionLaboratory;
-                } else if (params.laboratory) {
-                    laboratoryData.laboratory = params.laboratory;
-                }
-                data.laboratory = laboratoryData;
-            }
-            data.description = params.description
-            const previewPrescription = new PreviewPrescription(data);
-            await previewPrescription.save();
-            return res.send({ message: 'Receta Generada Exitosamente', previewPrescription });
+        data.description = params.description;
+        data.AnotherMedicaments = params.AnotherMedicaments;
+        data.AnotherLaboratories = params.AnotherLaboratories;
 
-        } else {
-            //Observa si los medicamentos existen
-            var medicamentExistStatus = true;
-            var laboratoryExistStatus = true;
-            for (let medicament of previewPrescriptionExist.medicament) {
-                if (medicament.name) {
-                    if (medicament.name.toLowerCase() == params.nameMedicament.toLowerCase()) {
-                        return res.status(400).send({ message: 'Este Medicamento ya esta en la receta.' });
-                    }
-                } else if (medicament.laboratory == params.laboratory) {
-                    return res.status(400).send({ message: 'Este Medicamento ya esta en la receta.' });
-                }
-            }
-            for (let laboratory of previewPrescriptionExist.laboratory) {
-                if (laboratory.name) {
-                    if (laboratory.name.toLowerCase() == params.nameLaboratory.toLowerCase()) {
-                        return res.status(400).send({ message: 'Este Laboratorio ya esta en la receta.' });
-                    }
-                } else if (laboratory.laboratory == params.laboratory) {
-                    return res.status(400).send({ message: 'Este Laboratorio ya esta en la receta.' });
-                }
-            }
-            if (!medicamentExistStatus) {
-                res.status(400).send({ message: 'El medicamento no existe' });
-            } else if (!laboratoryExistStatus) {
-                res.status(400).send({ message: 'El laboratorio no existe' })
-            }
-
-            const data = {}
-
-            if (medicamentExistStatus) {
-                const medicamentoData = {
-                    name: null,
-                    description: null,
-                    medicament: null
-                };
-                if (params.name) {
-                    medicamentoData.name = params.nameMedicament;
-                    medicamentoData.description = params.descriptionMedicament;
-                } else if (params.medicament) {
-                    medicamentoData.medicament = params.medicament;
-                }
-                data.medicament = medicamentoData;
-            }
-            if (laboratoryExistStatus) {
-                const laboratoryData = {
-                    name: null,
-                    description: null,
-                    laboratory: null
-                };
-                if (params.nameLaboratory) {
-                    laboratoryData.name = params.nameLaboratory;
-                    laboratoryData.description = params.descriptionLaboratory;
-                } else if (params.laboratory) {
-                    laboratoryData.laboratory = params.laboratory;
-                }
-                data.laboratory = laboratoryData;
-            }
-            const pushPrescriptionMedicament = await PreviewPrescription.findOneAndUpdate(
-                { _id: previewPrescriptionExist._id },
-                { $push: data },
-                { new: true }
-            );
-
-            if (!pushPrescriptionMedicament) return res.send({ message: 'Medicamento o Laboratorio no se añadio a la receta.' });
-            return res.send({ message: 'Nuevo Medicamento o Nuevo Laboratorio añadido exitosamente.', pushPrescriptionMedicament });
-
+        const previewPrescriptionNew = new PreviewPrescription(data);
+        await previewPrescriptionNew.save();
+        
+        if(!previewPrescriptionNew){
+            return res.status(400).send({ message: 'Receta No Generada'});
         }
-
-
-
+        return res.send({ message: 'Receta Generada Exitosamente', previewPrescriptionNew });
     } catch (err) {
         console.log(err);
         return res.status(500).send({ err, message: 'Error al Generar la Receta.' });
+    }
+}
+
+exports.addMedicamento = async (req,res) => {
+    try {
+        
+    }catch(err){
+        console.log(err);
+        return res.status(400).send({message: 'Error agregando medicamento a reseta '})
     }
 }
 
--- src/models/prescription.model.js
+++ src/models/prescription.model.js
@@ -5,24 +5,12 @@ const prescriptionSchema = mongoose.Schema({
     pacient: {type: mongoose.Schema.ObjectId, ref : 'User'},
     doctor: {type: mongoose.Schema.ObjectId, ref : 'Doctor'},
     medicament: 
-    [
-        {
-            name: String,
-            description: String,
-            medicament: {type: mongoose.Schema.ObjectId, ref : 'Medicament'}
-        }
-    ],
-    laboratory: 
-    [
-        {
-            name: String,
-            description: String,
-            laboratory: {type: mongoose.Schema.ObjectId, ref : 'Laboratory'}
-        }
-    ],
+    [{type: mongoose.Schema.ObjectId, ref : 'Medicament'}],
+    laboratory: [{type: mongoose.Schema.ObjectId, ref : 'Laboratory'}],
     description: String,
-    dateIssue: Date,
-    status: String,
+    AnotherMedicaments: String,
+    AnotherLaboratories: String,
+    dateIssue: Date
 });
 
 module.exports = mongoose.model('Prescription', prescriptionSchema);

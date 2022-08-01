e83028475aec3701dc2d42ebf1cc820e54681d6d
rcardenas-2020166 <rcardenas-2020166@kinal.edu.gt>
2022-08-01 02:19:39 -0600

    Se agregó el carrito de Compras Supremo Master XD

--- src/controllers/shoppingCart.controller.js
+++ src/controllers/shoppingCart.controller.js
@@ -1,11 +1,9 @@
 'use strict';
 
 
-//Importación del Modelo -Medicament-
 const Medicament = require('../models/medicament.model');
-
-//Importación del Modelo -Categories-
 const ShoppingCart = require('../models/shoppingCart.model');
+const User = require('../models/user.model');
 
 const { validateData, detailsShoppingCart } = require('../utils/validate');
 
@@ -15,98 +13,145 @@ exports.testShoppingCart = (req, res) => {
 }
 
 
-exports.createShoppingCart = async (req, res) => {
-    try {
+exports.createShoppingCart = async (req, res) => 
+{
+    try 
+    {
         const params = req.body;
         const user = req.user.sub;
 
-        const dataObligatoria = {
-            products: params.products,
+        const dataPrincipal = 
+        {
+            medicament: params.medicaments,
             quantity: params.quantity,
         }
 
-        const msg = validateData(dataObligatoria);
+        const msg = validateData(dataPrincipal);
         if (msg)
             return res.status(400).send(msg);
 
         const shoppingCartExist = await ShoppingCart.findOne({ user: user });
-        const productExist = await Medicament.findOne({ _id: params.products }).lean();
-        if (!productExist)
-            return res.send({ message: 'Medicamento no encontrado.' });
+        const medicamentExist = await Medicament.findOne({ _id: params.medicaments })
+        if (!medicamentExist)
+            return res.send({message: 'Medicamento no encontrado.'});
 
-        if (shoppingCartExist) {
-            if (params.quantity > productExist.stock)
-                return res.send({ message: 'No hay suficiente stock para este producto.' });
-            for (var key = 0; key < shoppingCartExist.products.length; key++) {
-                const idUpdateProduct = shoppingCartExist.products[key].product;
-                if (idUpdateProduct != params.products) continue;
-                return res.send({ message: 'Ya tienes este producto en el carrito.' });
+        if (shoppingCartExist) 
+        {
+            if (params.quantity > medicamentExist.stock)
+                return res.send({ message: 'No hay suficiente stock para este medicamento.' });
+            for (var key = 0; key < shoppingCartExist.medicaments.length; key++) 
+            {
+                const idUpdateMedicament = shoppingCartExist.medicaments[key].medicament;
+                if (idUpdateMedicament.valueOf() !== params.medicaments.valueOf()) continue;
+
+                const setMedicament = 
+                {
+                    medicament: params.medicaments,
+                    quantity: params.quantity,
+                    price: medicamentExist.price,
+                    subTotalMedicament: parseFloat(params.quantity) * parseFloat(medicamentExist.price)
+                }
+        
+                const dataSuma = 
+                {
+                    subTotal : setMedicament.subTotalMedicament,
+                    IVA : parseFloat(setMedicament.subTotalMedicament) * 0.12,
+                    total : parseFloat(setMedicament.subTotalMedicament) + parseFloat(setMedicament.subTotalMedicament) * 0.12,
+                }
+                 
+                const addNewMedicament = await ShoppingCart.findOneAndUpdate(
+                    { $and: [{ user: user }, { "medicaments.medicament": params.medicaments }] },
+                    {
+                        $inc:
+                        {
+                            "medicaments.$.quantity": params.quantity,
+                            "medicaments.$.subTotalMedicament": setMedicament.subTotalMedicament,
+                            subTotal : dataSuma.subTotal,
+                            IVA: dataSuma.IVA,
+                            total: dataSuma.total
+                        },
+                    },
+                    { new: true }).lean();
+                return res.send({ message: 'Cantidad del Producto Actualizada', addNewMedicament });
             }
-            const setProduct = {
-                medicament: params.products,
+            const setMedicament = 
+            {
+                medicament: params.medicaments,
                 quantity: params.quantity,
-                price: productExist.price,
-                subTotalProduct: parseFloat(params.quantity) * parseFloat(productExist.price)
+                price: medicamentExist.price,
+                subTotalMedicament: parseFloat(params.quantity) * parseFloat(medicamentExist.price)
             }
-            const subTotal = shoppingCartExist.products.map(item =>
-                item.subTotalProduct).reduce((prev, curr) => prev + curr, 0) + setProduct.subTotalProduct;
+            const subTotal = shoppingCartExist.medicaments.map(item =>
+                item.subTotalMedicament).reduce((prev, curr) => prev + curr, 0) + setMedicament.subTotalMedicament;
             const IVA = parseFloat(subTotal) * 0.12;
             const total = parseFloat(subTotal) + parseFloat(IVA);
 
             const newShoppingCart = await ShoppingCart.findOneAndUpdate({ _id: shoppingCartExist._id },
                 {
-                    $push: { products: setProduct },
+                    $push: { medicaments: setMedicament },
                     subTotal: subTotal,
                     IVA: IVA,
                     total: total
                 },
                 { new: true });
-            const shoppingCart = await detailsShoppingCart(newShoppingCart._id);
-            return res.send({ message: 'Producto nuevo agregado al carrito de compras.', shoppingCart })
+            return res.send({ message: 'Medicamento añadido.', newShoppingCart })
         }
-        if (params.quantity > productExist.stock)
-            return res.send({ message: 'No hay suficiente stock para este producto.' });
 
-        const setProduct = {
-            medicament: params.products,
+       if (params.quantity > medicamentExist.stock)
+            return res.send({ message: 'No hay suficiente stock para este medicamento.' });
+
+        const setMedicament = 
+        {
+            medicament: params.medicaments,
             quantity: params.quantity,
-            price: productExist.price,
-            subTotalProduct: parseFloat(params.quantity) * parseFloat(productExist.price)
+            price: medicamentExist.price,
+            subTotalMedicament: parseFloat(params.quantity) * parseFloat(medicamentExist.price)
         }
 
-        const data = {
+        const data = 
+        {
             user: req.user.sub,
-            products: setProduct
+            medicaments: setMedicament
         }
-        data.subTotal = setProduct.subTotalProduct
+
+        data.subTotal = setMedicament.subTotalMedicament
         data.IVA = parseFloat(data.subTotal) * 0.12;
         data.total = parseFloat(data.subTotal) + parseFloat(data.IVA);
-        if (params.NIT == '' || params.NIT == undefined || params.NIT == null) {
+
+        //BUSCAR NIT DEL USUARIO//
+        const searchUser = await User.findOne({_id:user})
+        if (searchUser.NIT == '' || searchUser.NIT == undefined || searchUser.NIT == null) 
+        {
             data.NIT = 'C/F'
         }
-        else { data.NIT = params.NIT }
+        else { data.NIT = searchUser.NIT }
 
         const addShoppingCart = new ShoppingCart(data);
         await addShoppingCart.save();
-        const shoppingCart = await detailsShoppingCart(addShoppingCart._id);
-        return res.send({ message: 'Producto nuevo añadido.', shoppingCart });
-
-    } catch (err) {
+        return res.send({ message: 'Medicamento añadido.', addShoppingCart });
+    } 
+    catch (err) 
+    {
         console.log(err);
-        return res.status(500).send({ err, message: 'Error al crear el Shopping Cart.' });
+        return res.status(500).send({ err, message: 'Error al crear el Carrito de Compras.' });
     }
 }
 
 exports.getShoppingCart = async (req, res) => {
-    try {
+    try 
+    {
         const user = req.user.sub;
 
-        const cart = await ShoppingCart.findOne({ user: user }).populate('user')
-        if (!cart) {
+        const shoppingCart = await ShoppingCart.findOne({ user: user }).populate('user medicaments.medicament')
+        if (!shoppingCart)
+        {
             return res.status(404).send({ message: 'No se encontro nada.' });
-        } else return res.send({ message: 'Este es tu carrito de compras. ', cart });
+        } 
+        else return res.send({ message: 'Este es tu carrito de compras. ', shoppingCart });
 
-    } catch (err) {
+    } 
+    catch (err) 
+    {
         console.log(err);
         return res.status(500).send({ err, message: 'Error al obtener el Shopping Cart.' });
     }
